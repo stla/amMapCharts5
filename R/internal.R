097e97648948d822e3df3a98a8798118351042c7
stla <laurent_step@yahoo.fr>
2024-03-22 12:31:37 +0100

    cssColors

--- R/internal.R
+++ R/internal.R
@@ -12,15 +12,23 @@ regex_hsl <- paste0("^hsl\\(",
                     "(", regex_255, "),",
                     "(", regex_255, ")\\)$")
 
-cssColors <- c("transparent", "aqua", "crimson", "fuchsia", "indigo", "lime",
+cssColors <- c("aqua", "crimson", "fuchsia", "indigo", "lime",
                "olive", "rebeccapurple", "silver", "teal")
 
 #' @importFrom grDevices col2rgb rgb
+#' @importFrom utils read.csv
 #' @noRd
 validateColor <- function(color){
   if(is.null(color)) return(NULL)
-  if(grepl(regex_rgb, color) || grepl(regex_hsl, color) || color %in% cssColors){
+  if(grepl(regex_rgb, color) || grepl(regex_hsl, color)){
     return(color)
+  }
+  if(color %in% cssColors) {
+    colorTable <- read.csv(
+      system.file("cssColors", "cssColors.csv", package = "amMapCharts5")
+    )
+    i <- which(colorTable[["color"]] == tolower(color))
+    return(colorTable[["hex"]][i])
   }
   RGB <- col2rgb(color)[,1]
   rgb(RGB["red"], RGB["green"], RGB["blue"], maxColorValue = 255)
